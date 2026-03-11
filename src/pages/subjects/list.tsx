b8ac7de4850f5da877641aa4bd1060b75daad704
amid-hassan <amidhassan589@gamil.com>
2026-03-11 21:55:38 +0530

    implement data provider

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,8 +1,122 @@
-import React from 'react'
+import React, {useMemo, useState} from 'react'
+import {ListView} from "@/components/refine-ui/views/list-view.tsx";
+import {Breadcrumb} from "@/components/refine-ui/layout/breadcrumb.tsx";
+import {Search} from "lucide-react";
+import {Input} from "@/components/ui/input.tsx";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.tsx";
+import {DEPARTMENT_OPTIONS} from "@/constants";
+import {CreateButton} from "@/components/refine-ui/buttons/create.tsx";
+import {DataTable} from "@/components/refine-ui/data-table/data-table.tsx";
+import {useTable} from "@refinedev/react-table";
+import {Subject} from "@/types ";
+import {Badge} from "@/components/ui/badge.tsx";
+import {ColumnDef} from "@tanstack/react-table";
 
 const SubjectsList = () => {
+    const [searchQuery, setSearchQuery] = useState('')
+    const [selectedDepartment, setSelectedDepartment] = useState('all')
+
+    const departmentFilters = selectedDepartment == 'all' ? [] : [
+        { field: 'department', operator: 'eq' as const, value: selectedDepartment }
+    ];
+    const searchFilters = searchQuery ? [
+        { field: 'name', operator: 'contains' as const, value: searchQuery}
+    ] : [];
+
+    const subjectTable = useTable<Subject>({
+        columns: useMemo<ColumnDef<Subject>[]>(() =>[
+            {
+                id: 'code',
+                accessorKey: 'code',
+                size: 100,
+                header: () => <p className="coloumn-title ml-2">Code</p>,
+                cell: ({ getValue}) => <Badge>{getValue<String>()}</Badge>
+            },
+            {
+                id: 'name',
+                accessorKey: 'name',
+                size: 200,
+                header: () => <p className="coloumn-title">Name</p>,
+                cell: ({ getValue}) => <span className={"text-foreground"}>{getValue<String>()}</span>,
+                filterFn: 'includesString'
+            },
+            {
+                id: 'department',
+                accessorKey: 'department',
+                size: 150,
+                header: () => <p className="coloumn-title">Department</p>,
+                cell: ({ getValue}) => <Badge variant="secondary">{getValue<String>()}</Badge>,
+            },
+            {
+                id: 'description',
+                accessorKey: 'description',
+                size: 300,
+                cell: ({ getValue}) => <span className="turncate line-clamp-2">{getValue<string>()}</span>,
+            }
+        ], []),
+        refineCoreProps:{
+            resource: 'subjects',
+            pagination: { pageSize: 10, mode: 'server'},
+            filters: {
+                permanent: [...departmentFilters, ...searchFilters]
+            },
+            sorters: {
+                initial: [
+                    { field: 'id', order: 'desc'},
+                ]
+            },
+        }
+    });
     return (
-        <div>List</div>
+        <ListView>
+            <Breadcrumb/>
+
+            <h1 className="page-title">Subjects</h1>
+
+            <div className="intro-row">
+                <p>Quick access to essential metrics and management tools.</p>
+
+                <div className="action-row">
+                    <div className="search-filed">
+                        <Search className="search-icon"/>
+
+                        <Input
+                            type="text"
+                            placeholder="Search by name..."
+                            value={searchQuery}
+                            onChange={(e)  => setSearchQuery(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="flex gap-2 w-full sm:w-auto">
+                        <Select
+                            value={selectedDepartment}
+                            onValueChange={setSelectedDepartment}
+                        >
+                            <SelectTrigger>
+                                <SelectValue placeholder="Fliter by department" />
+                            </SelectTrigger>
+
+                            <SelectContent>
+                                <SelectItem value='all'>
+                                    All Departments
+                                </SelectItem>
+                                {DEPARTMENT_OPTIONS.map(department => (
+                                    <SelectItem key={department.value}
+                                                value={department.value}>
+                                        {department.label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+
+                        <CreateButton/>
+                    </div>
+                </div>
+            </div>
+
+            <DataTable table={subjectTable} />
+        </ListView>
     )
 }
 
